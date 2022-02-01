5f2b91ddc4caea059b32ad7d62702a05bd56fbe4
hutch2323 <marcus.hutchings30@gmail.com>
2022-02-01 12:16:00 -0330

    Updates: 2022-02-01

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -31,13 +31,24 @@ const __dirname = dirname(__filename);
 // })
 
 const storage = multer.diskStorage({
-    destination: "./src/build/images/",
+    // destination: "./build/images/",
+    destination: "src/build/images/",
     filename: (req, file, cb) => {
         const fileName = file.originalname.toLowerCase().split(' ').join('-');
         cb(null, fileName)
     }
 });
 
+const storageJSON = multer.diskStorage({
+    // destination: "./build/images/",
+    destination: "",
+    filename: (req, file, cb) => {
+        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+        cb(null, fileName)
+    }
+});
+
+const uploadMovies = multer({});
 const upload = multer({storage: storage});
 // var upload = multer({
 //     storage: storage,
@@ -66,7 +77,7 @@ const uploadFiles = async (req, res) => {
 
         //const movieInfo = await db.collection('movies').find({name:req.params.name}).toArray();
         await db.collection('movies').insertOne({name:req.body.name, date:req.body.date, actors:req.body.actors.split(', '), poster:req.file.path.slice(req.file.path.indexOf("\\images")), rating:req.body.rating});
-
+        // await db.collection('movies').insertOne({name:req.body.name, date:req.body.date, actors:req.body.actors.split(', '), poster:req.file.path, rating:req.body.rating});
         // console.log(movieInfo);
         const movieInfo = await db.collection('movies').find({}).toArray();
         res.status(200).json({message:"Success", movies: movieInfo});
@@ -78,11 +89,63 @@ const uploadFiles = async (req, res) => {
 
 }
 
+// const uploadMovieData = async (req, res) => {
+//     try{
+//         console.log("File content:")
+//         console.log(req.body);
+//         const client = await MongoClient.connect('mongodb://localhost:27017', {useNewUrlParser: true})
+//         const db = client.db("movies");
+
+//         //const movieInfo = await db.collection('movies').find({name:req.params.name}).toArray();
+//         let movieData = undefined;
+//         fs.readFile(req.file, "utf8", (err, data) => {
+//             console.log(err)
+//             console.log("File Contents:")
+//             console.log(data)
+//             movieData = data;
+//         });
+//         // await db.collection('movies').insertMany(movieData);
+//         // await db.collection('movies').insertOne({name:req.body.name, date:req.body.date, actors:req.body.actors.split(', '), poster:req.file.path, rating:req.body.rating});
+//         // console.log(movieInfo);
+//         // const movieInfo = await db.collection('movies').find({}).toArray();
+//         // res.status(200).json({message:"Success", movies: movieInfo});
+//         client.close();
+//     }
+//     catch (error) {
+//         res.status(500).json({message: "Error connecting to db", error});
+//     }
+
+// }
+
 app.post('/api/addMovie', upload.single("poster"), uploadFiles);
+
+app.post('/api/addMovies', async(req, res) => {
+    const apiKey = "ef72570ff371408f9668e414353b7b2e";
+    console.log(req.headers);
+
+    if (req.headers.apikey == apiKey) {
+        try{
+            let movieData = req.body
+            const client = await MongoClient.connect('mongodb://localhost:27017', {useNewUrlParser: true})
+            const db = client.db("movies");
+    
+            await db.collection('movies').insertMany(movieData);
+            res.status(200).json({message:"Success", movies: req.body});
+            client.close();
+        }
+        catch (error) {
+            res.status(500).json({message: "Error connecting to db", error});
+        }
+    } else {
+        res.status(500).json({message: "Invalid API Key"});
+    }
+
+});
 
 app.post('/api/removeMovie', async (req, res) => {
     try{
-        console.log("We're in the removeMovie API")
+        upload.single("poster");
+        console.log("We're in the removeMovie API");
         console.log(req.body.name);
         const client = await MongoClient.connect('mongodb://localhost:27017', {useNewUrlParser: true})
         const db = client.db("movies");
@@ -90,7 +153,8 @@ app.post('/api/removeMovie', async (req, res) => {
         // to delete the image file associated with the movie
         const movieInfo = await db.collection('movies').findOne({name:req.body.name})
         console.log(movieInfo);
-        fs.unlink("./src/build"+movieInfo.poster, (err) => {
+        //fs.unlink("./src/build"+movieInfo.poster, (err) => {
+        fs.unlink(movieInfo.poster, (err) => {
             if (err) {
               console.error(err)
               return
